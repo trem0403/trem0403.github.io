00571c284c0dd34ff0e959244d21239e9053fd27
trem0403 <156836799+trem0403@users.noreply.github.com>
2024-11-12 16:30:36 -0500

    Version 1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function dateTime() {
+	const date = new Date();
+	let today = date.toDateString();
+	let time = date.toLocaleTimeString();
+	document.getElementById('date-time').innerHTML = '<p id="date">' + today + '</p><p id="time">' + time + '</p>';
+	setTimeout(dateTime, 1000);
+}
+
+function weatherBalloon(cityID) {
+	var apiKey = '6f12a77cdba4141499533c1880e7b349'; //OpenWeather API key
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey)
+		.then(function(resp) {
+			return resp.json()
+		})
+		.then(function(data) {
+			let weatherIcon = data.weather[0].icon;
+			let tempK = parseFloat(data.main.temp);
+			let tempC = Math.round(tempK - 273.15);
+			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
+			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempF + '&deg;F">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempF + '&deg;F</p>';
+		});
+}
+
+function laputa() {
+	dateTime();
+  	weatherBalloon(6094817); //OpenWeather city ID
+}
